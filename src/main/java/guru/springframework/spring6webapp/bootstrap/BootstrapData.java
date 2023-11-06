404d4484670b41101fbc1415f3722eb2f889f538
Jakov Delas <delas.jakov@feg.eu>
2023-11-06 15:25:58 +0100

    initial commit

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,43 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123456");
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+        ericSaved.getBooks().add(dddSaved);
+
+        authorRepository.save(ericSaved);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+
+    }
+}
